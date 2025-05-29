ac894c20428c1e54b9398497178a4beaf70f9869
ADYASHA AUROSIKHA NAYAK <126941709+Adyashaa22@users.noreply.github.com>
2025-05-29 19:55:01 +0530

    Update __init__.py

--- flask_template/app/__init__.py
+++ flask_template/app/__init__.py
@@ -12,7 +12,7 @@ def create_app(config_class=Config):
     app.register_blueprint(main_bp)
     # Set the database path relative to this file
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    DB_PATH = os.path.abspath(os.path.join(BASE_DIR, '..', 'movie_spoilers_db.json'))
+    DB_PATH = os.path.abspath(os.path.join(BASE_DIR, '..', '..', 'movie_spoilers_db.json'))
     app.config['MOVIE_DB_PATH'] = DB_PATH
     
     # Create necessary directories if they don't exist
